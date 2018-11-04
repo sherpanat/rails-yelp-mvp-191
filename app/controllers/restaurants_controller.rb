d38d2404512c85568c650cc7198be7bbed417b0e
Nathan Huberty <nathan.huberty@orange.fr>
2018-11-04 18:05:38 +0100

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,33 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :index]
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
+    @categories = Restaurant::CATEGORIES
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :phone_number, :address)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.new
   end
 end
